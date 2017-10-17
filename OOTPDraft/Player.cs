abd5747d0f09e7dfdea89b6512090736f25c8607
kiebles1 <kiebles1@gmail.com>
2017-10-16 20:19:54 -0400

    Added source files

---
+++ OOTPDraft/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOTPDraft
+{
+    class Player
+    {
+        public string mName;
+        public double mPotentialAvg;
+        public int mScoutPotential;
+        public int mOOTPPotential;
+
+        private const double SCOUT_WEIGHT = 0.75;
+        private const double OOTP_WEIGHT = 0.25;
+
+        public Player(string pName, int pScoutPotential, int pOOTPPotential)
+        {
+            mName = pName;
+            mPotentialAvg = GetPotentialAvg(pScoutPotential, pOOTPPotential);
+            mOOTPPotential = pOOTPPotential;
+            mScoutPotential = pScoutPotential;
+            
+        }
+
+        static double GetPotentialAvg(int pScoutPotential, int pOOTPPotential)
+        {
+            double potentialAvg = ((double)pScoutPotential * 0.75 + (double)pOOTPPotential * 0.25) / (2);
+            return potentialAvg;
+        }
+
+    }
+}
---
+++ OOTPDraft/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace OOTPDraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fStream = File.OpenText(@".\DraftData.csv");
+            fStream.ReadLine();
+            fStream.ReadLine();
+            List <Player> lDraftList = new List<Player>();
+            string[] lPlayerString = fStream.ReadLine().Split(',');
+            while(!lPlayerString[14].Equals("")) {
+                int lScoutPotential = int.Parse(lPlayerString[13][0].ToString());
+                int lOOTPPotential = int.Parse(lPlayerString[14][0].ToString());
+                lDraftList.Add(new Player(lPlayerString[3], lScoutPotential, lOOTPPotential));
+                lPlayerString = fStream.ReadLine().Split(',');
+            }
+            lDraftList = lDraftList.OrderByDescending(p => p.mPotentialAvg).
+                ThenByDescending(p => p.mScoutPotential).ToList();
+            StreamWriter fOutStream = new StreamWriter(@".\DraftOutput.csv");
+            foreach (Player p in lDraftList) {
+                fOutStream.WriteLine(p.mName + "," + p.mPotentialAvg.ToString() + "," +
+                    p.mScoutPotential.ToString() + "," + p.mOOTPPotential);
+            }
+            Console.Read();
+            fStream.Close();
+            fOutStream.Close();
+        }
+    }
+}
